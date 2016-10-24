623a43317af6fbe03cd7a36e400913082c8aec6f
Pekechan <efboneu@gmail.com>
2016-10-24 21:34:57 +0200

    arreglada la busqueda

--- autopeli.py
+++ autopeli.py
@@ -4,15 +4,22 @@ import urllib2  # pedir url donde buscar
 
 print sys.argv
 
+# Comprobamos que han puesto el nombre
 if len(sys.argv) < 2:
-    sys.exit ('falta el nombre de la peli')
+    print 'falta el nombre de la peli'
+    sys.exit(1)
 
-       
 name = sys.argv[1]
-response = urllib2.urlopen('http://www.mejortorrent.com/secciones.php?sec=buscador&valor=' + name)
 
-if response.getcode() == 200:
-    print response.read()
-else:
-     print response.getcode()
+# Hacemos la busqueda en la pagina
+response = urllib2.urlopen('http://www.mejortorrent.com/secciones.php?sec=buscador&valor=' + str.replace(name,' ','+'))
 
+# Comprobamos que el proveedor nos contesta
+if response.getcode() != 200:
+    print 'error de la web del proveedor: {}'.format(response.getcode())
+    sys.exit(1)
+
+# Damos respuesta a nuestro cliente
+print response.read()
+    
+    
